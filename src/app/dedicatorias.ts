6eee4869f636797d067d0c847a7ebbc701a3695a
lettertreslineas <65137396+lettertreslineas@users.noreply.github.com>
2022-09-04 18:26:29 -0500

    6 files modified

--- src/app/dedicatorias.ts
+++ src/app/dedicatorias.ts
@@ -1,13 +1,29 @@
 export interface dedicatoria {
   id : number;
   name: string;
+  foto: string;
   descripcion: string;
+  espacio: string;
 
 }
 
 export const dedicatorias = [{
   id : 1,
   name : 'pienso en ti',
-  descripcion : 'eres lo mas preciado'
+  foto :'https://scontent.fbga1-4.fna.fbcdn.net/v/t1.6435-9/42608173_1952538904807930_2815061397048655872_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=e3f864&_nc_ohc=ZJrdBHtXQh4AX_HvHVf&_nc_ht=scontent.fbga1-4.fna&oh=00_AT9m43vqwes4oq-5W5U2FqGdV2tMFv52rWo3uL0d0BbPnQ&oe=63392FDC',
+  descripcion : 'eres lo mas preciado',
+  espacio:'...'
+},
+
+{
+  id : 2,
+  name : 'pienso en ti',
+  foto :"https://scontent.fbga1-4.fna.fbcdn.net/v/t39.30808-6/279064191_5316923155036138_1464898180340717139_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5CG1aa-22jkAX9y_IEW&_nc_oc=AQnZC9Ekuvkfobdx_1-zWAIGymoXkk957tbntDf_uFaPYh_XKW3Q2mCk8QhDaDs7H2Q&_nc_ht=scontent.fbga1-4.fna&oh=00_AT9xr1o1wgI_8sBC1zLRSYKiNh-uqzDnKgNCb2wQaK_7jQ&oe=631A63F0",
+  descripcion : 'eres lo mas preciado',
+  espacio:'...'
+
 }
+
+
+
 ]
