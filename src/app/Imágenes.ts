e6c39af55a3385fdb2fd265f695bff8c9097aa89
lettertreslineas <65137396+lettertreslineas@users.noreply.github.com>
2022-09-07 10:22:53 -0500

    2 files modified, 1 added

---
+++ src/app/Imágenes.ts
@@ -0,0 +1,9 @@
+export interface imagene {
+  logo:string
+}
+export const imagenes = [{
+logo:"https://pixabay.com/es/vectors/search/desplazarse/",
+
+}
+
+]
--- src/app/parte-superior/parte-superior.component.ts
+++ src/app/parte-superior/parte-superior.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { imagenes } from '../Imágenes';
 
 @Component({
   selector: 'app-parte-superior',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParteSuperiorComponent implements OnInit {
 
-
+imagenes = imagenes;
 
   constructor() { }
 
