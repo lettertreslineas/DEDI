9d876d66f9960b744a93fa871c1b13f544be3bd0
lettertreslineas <65137396+lettertreslineas@users.noreply.github.com>
2022-09-06 20:47:35 -0500

    1 file modified

--- src/app/dedicatorias.ts
+++ src/app/dedicatorias.ts
@@ -14,7 +14,7 @@ export const dedicatorias = [{
   de : 'lim',
   para : 'familia r',
   name : 'my family',
-  foto :'',
+  foto :'https://cdn.pixabay.com/photo/2017/11/10/08/10/mother-2935723__340.jpg',
   descripcion : 'lo mas lindo que se puede tener una compañia incomparable',
   espacio:'...'
 },
@@ -24,12 +24,21 @@ export const dedicatorias = [{
   de: 'lix',
   para: 'hermana',
   name : 'pienso en ti',
-  foto :"",
+  foto :"https://cdn.pixabay.com/photo/2015/12/03/02/21/child-1073638__340.jpg",
   descripcion : 'hermana quizas estes lejos pero siempre pienso en que ests cerca siempre te llevo en mi corazon.',
   espacio:'...'
 
+},
+
+{
+  id : 3,
+  de: 'alcon',
+  para: 'mi amor',
+  name : 'cada dia te quiero mas',
+  foto :"https://cdn.pixabay.com/photo/2018/08/13/03/21/family-3602245__340.jpg",
+  descripcion : 'gracias por ser parte de mi vida, me sobran las palabras para expresarte todo lo que siento por ti.',
+  espacio:'...'
+
 }
-
-
 
 ]
